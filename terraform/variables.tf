99af7f08e7c03709cce8ec3c4d5ca336ddac6bd0
Alec Moschetti <alecmoschetti@gmail.com>
2026-06-09 14:38:14 -0700

    Added cidr and aws_region terraform variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,6 +7,11 @@ variable "project_name" {
 # ***********************************************
 # AWS instance configuration variables
 # ***********************************************
+variable "aws_region" {
+  description = "AWS region to deploy into"
+  type        = string
+  default     = "us-east-1"
+}
 
 variable "instance_type" {
   description = "t3.medium is recommended for minimum Minecraft server hardware requirements"
@@ -18,4 +23,14 @@ variable "volume_size" {
   description = "Root volume size in GB"
   type = number
   default = 20
+}
+
+variable "allowed_ssh_cidr" {
+  description = <<-EOT
+    CIDR block allowed to reach port 22 (SSH).
+    Set to your own IP for tighter security: e.g. \"203.0.113.5/32\".
+    Defaults to open (0.0.0.0/0) for convenience in lab environments.
+  EOT
+  type        = string
+  default     = "0.0.0.0/0"
 }
